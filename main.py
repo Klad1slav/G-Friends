5d5a1c12f59a8c9ad4e2c437887fbeaa8e2599e0
Klad1slav <vladmajstrenko13@gmail.com>
2024-10-04 21:08:09 +0300

    reg

--- main.py
+++ main.py
@@ -1,33 +1,74 @@
 import telebot
-import sqlite3
+import sqlite3 as sq
 from telebot import types
 
 bot = telebot.TeleBot('7155989263:AAFcqabkraoHUo7DnPQsi51Fh0kBPXk6Vrg')
 name = None
+age = None
+gender = None
+AboutMe = None
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    conn = sqlite3.connect('G!Friends.sql')
-    cur = conn.cursor()
+    with sq.connect('G!Friends.db', check_same_thread=False) as con:
+     cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, name, age, gender, AboutMe )')
 
-    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name TEXT, age INTEGER, gender TEXT, AboutME Text )')
-   #cur.execute('CREATE TABLE IF NOT EXISTS users_preference (id int auto_increment primary key,gender_preference TEXT, age_min INTEGER, age_max INTEGER, FOREIGN KEY (id) REFERENCES users (id)')
-    conn.commit()
-    cur.close()
-    conn.close()
+    con.commit()
 
-    bot.send_message(message.chat.id, 'Привет , давай создадим новый аккаунт! Введите ваше имя! ')
+    bot.send_message(message.chat.id, 'Привет , давай создадим новый аккаунт! Введите ваше имя')
     bot.register_next_step_handler(message, user_name)
 
+def user_name(message):
+    global name
+    name = message.text.strip()
 
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('1')
-    btn2 = types.KeyboardButton('2')
-    btn3 = types.KeyboardButton('3')
-    btn4 = types.KeyboardButton('4')
-    markup.row(btn1, btn2, btn3, btn4)
-    #bot.send_message(message.chat.id, 'Привет , давай создадим новый аккаунт! '+ message.from_user.first_name, reply_markup = markup)
+    bot.send_message(message.chat.id, 'Введите ваш возраст!')
+    bot.register_next_step_handler(message, user_age)
+
+def user_age(message):
+    global age
+    age = message.text.strip()
+
+    bot.send_message(message.chat.id, '''Ваш пол?
+    1.Мужской
+    2.Женский''')
+
+    bot.register_next_step_handler(message, user_gender)
+
+def user_gender(message):
+    global gender
+    gender = message.text.strip()
+
+    bot.send_message(message.chat.id, 'Расскажите немного про себя')
+    bot.register_next_step_handler(message, user_AboutMe)
+
+def user_AboutMe(message):
+    global AboutMe
+    AboutMe = message.text.strip()
+
+    bot.send_message(message.chat.id, 'Отличная анкета получилось')
+
+
+    with sq.connect('G!Friends.db', check_same_thread=False) as con:
+        cur = con.cursor()
+
+        cur.execute("INSERT INTO users (name, age, gender, AboutMe ) VALUES ('%s','%s','%s','%s')" % (name, age, gender, AboutMe))
+        con.commit()
+      #  cur.close()
+       # con.close()
+
+    bot.register_next_step_handler(message, user_FinReg)
+
+def user_FinReg(message):
+
+
+        bot.send_message(message.chat.id, 'Все верно?')
+        bot.register_next_step_handler(message, menu)
+
+@bot.message_handler(commands=['menu'])
+def menu(message):
     bot.send_message(message.chat.id,
 '''
 1.Смотреть анкеты.
@@ -35,39 +76,27 @@ def start(message):
 3.Изменить фото/видео.
 4.Изменить текст анкеты.
     ''')
-
-def user_name(message):
-    global name
-    name = message.text.strip()
-    bot.send_message(message.chat.id, 'Введите ваш возраст! ')
-    bot.register_next_step_handler(message, user_age)
-
-def user_age(message):
-    age = message.text.strip()
-
-    conn = sqlite3.connect('G!Friends.sql')
-    cur = conn.cursor()
-
-    cur.execute('INSERT INTO users (name, age) VALUES ("%s","%s")' % (name, age))
-    conn.commit()
-    cur.close()
-    conn.close()
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('1')
+    btn2 = types.KeyboardButton('2')
+    btn3 = types.KeyboardButton('3')
+    btn4 = types.KeyboardButton('4')
+    markup.row(btn1, btn2, btn3, btn4)
 
 
 
 
 
-@bot.message_handler(commands=['help'])
-def help(message):
-  bot.send_message(message.chat.id, 'Help!')
 
-@bot.message_handler(commands=['userId'])
-def userId(message):
-      bot.send_message(message.chat.id, 'Help!')
 
-@bot.message_handler(content_types=['photo'])
-def get_photo(message):
-    bot.reply_to(message, 'Фото')
+
+
+   # cur.execute('INSERT INTO users (name, age) VALUES ("%s","%s")' % (name, age))
+   # conn.commit()
+   # cur.close()
+   # conn.close()
+
+
 
 
 bot.polling(none_stop=True)
